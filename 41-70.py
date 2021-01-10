d2d843f4379be8214d7bc6b183af12032ff5b765
leeyoungjun1113 <leeyoungjun1113@gmail.com>
2021-01-10 20:50:41 +0900

    41-70

---
+++ 41-70.py
@@ -0,0 +1,47 @@
+# data = "   삼성전자    "
+# data1 = data.strip()
+# print(data1)
+# print(data)
+
+# data = "    039490"
+# data = data.strip()
+# print(data)
+
+# file_name = "보고서.xlsx"
+# print(file_name.endswith(("xlssx", "xle")))
+
+# a = "hello_kity"
+# print(a.split("_"))
+
+# date = "2020-01-10"
+# print(date.split("-"))
+
+# movie_rank = ['닥터 스트레인지', '스플릿', '럭키', '배트맨']
+# movie_rank.insert(1, "슈퍼맨")
+# print(movie_rank)
+
+# movie_rank = ['닥터 스트레인지', '스플릿', '럭키', '배트맨']
+# del movie_rank[0]
+# print(movie_rank)
+
+# cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+# print(len(cook))
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(nums[::])
+
+# interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+# print("\n".join(interest))
+
+# string = "삼성전자,LG전자,Naver"
+# interest = string.split(",")
+# print(interest)
+
+# data = [2, 4, 3, 1, 5, 10, 9]
+# data.sort()
+# print(data)
+# data = [2, 4, 3, 1, 5, 10, 9]
+# data2 = sorted(data)
+# print(data2)
+# print(data)
+git status
